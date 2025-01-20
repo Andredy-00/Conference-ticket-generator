1110f24c2b8f3bde795e40037b01480a06000c7d
Andredy Guerra <andredyguerra00@gmail.com>
2025-01-20 16:58:48 -0500

    80% de terminar el formulario

--- src/app/ui/crate-form.tsx
+++ src/app/ui/crate-form.tsx
@@ -2,14 +2,13 @@
 
 import Image from "next/image";
 import iconUpload from "../../../public/assets/images/icon-upload.svg";
-import iconInfo from "../../../public/assets/images/icon-info.svg"
+import iconInfo from "../../../public/assets/images/icon-info.svg";
 
 export default function Form() {
   return (
     <form action="" className="w-full">
       {/* Upload avatar */}
       <div className="text-white p-3 font-medium grid gap-3">
-
         {/* File input */}
         <h2 className="capitalize">upload Avatar</h2>
         <div className="border-dashed border-2 border-gris w-full rounded-lg p-3 background-file">
@@ -27,35 +26,34 @@ export default function Form() {
         </div>
         <input className="hidden" type="file" id="file-input" />
         <div className="flex gap-2 items-center">
-          <Image
-            src={iconInfo}
-            alt="Informacion"
-          />
-        <p className="text-xs">
-          Upload your photo (JPG or PNG, max size: 500KB
-        </p>
+          <Image src={iconInfo} alt="Informacion" />
+          <p className="text-xs">
+            Upload your photo (JPG or PNG, max size: 500KB
+          </p>
         </div>
       </div>
+              
+      <div className="p-3 grid gap-3 capitalize text-white">
+        {/* Full name */}
+        <div className="input-style">
+          <label htmlFor="fullname">full name</label>
+          <input type="text" id="fullname" />
+        </div>
 
-      {/* Full name */}
-      <div>
-        <label htmlFor="fullname">full name</label>
-        <input type="text" id="fullname" />
+        {/* Email Address */}
+        <div className="input-style">
+          <label htmlFor="email">email address</label>
+          <input type="email" id="email" placeholder="example@gmail.com" />
+        </div>
+
+        {/* GitHub Username */}
+        <div className="input-style">
+          <label htmlFor="github-username">GitHub username</label>
+          <input type="text" id="github-username" placeholder="@yourusername" />
+        </div>
+
+        <input type="submit" value="Generate My Ticket" className="btn-submit"/>
       </div>
-
-      {/* Email Address */}
-      <div>
-        <label htmlFor="email">email address</label>
-        <input type="email" id="email" placeholder="example@gmail.com" />
-      </div>
-
-      {/* GitHub Username */}
-      <div>
-        <label htmlFor="github-username">GitHub username</label>
-        <input type="text" id="github-username" />
-      </div>
-
-      <input type="suubmit" value="Generate My Ticket" />
     </form>
   );
 }
